9ba5022214adb8b95ce31f3dd1d70416b75c3966
erictu22 <erictu222@gmail.com>
2023-05-15 14:10:28 -0400

    Adds a header section

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -1,13 +1,20 @@
 import styled from 'styled-components'
 
+export const Title = styled.h1`
+    margin: 0;
+    font-size: 60px;
+`
+
 export const H2 = styled.h2`
-    
+    margin: 0;
 `
 
 export const H3 = styled.h3`
-    
+    margin: 0;
+    font-size: 28px;
+    font-weight: 600;
 `
 
 export const P = styled.p`
-
+    margin: 0;
 `
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,46 @@
 import CircleImage from "../components/circleImage";
-import { FlexRow } from "../components/flex";
-import logo from "../logo.svg";
+import { FlexColumn, FlexRow } from "../components/flex";
+import { H2, H3, P, Title } from "../components/typography";
+import { useIsMobileLayout } from "../hooks/useIsMobileLayout";
 
 export function Home() {
   return (
     <div className="App">
-      <FlexRow style={{ justifyContent: "center" }}>
-        <CircleImage
-          src="./profile_square.png"
-          alt="A picture of Eric Tu"
-          size={42}
-        />
-      </FlexRow>
+      <HeaderSection />
     </div>
   );
 }
+
+const HeaderSection = () => {
+  const headerSpacing = 120;
+  const isMobileLayout = useIsMobileLayout();
+
+  return (
+    <FlexRow
+      style={{
+        justifyContent: "center",
+        marginTop: headerSpacing,
+        marginBottom: headerSpacing,
+      }}
+    >
+      <FlexRow>
+        {isMobileLayout ? null : (
+          <CircleImage
+            src="./profile_square.png"
+            alt="A picture of Eric Tu"
+            size={180}
+          />
+        )}
+
+        <FlexColumn style={{ marginLeft: 60, justifyContent: "center" }}>
+          <Title style={{ textAlign: "start", marginBottom: 12 }}>
+            Hi! I'm Eric 👋
+          </Title>
+          <H3 style={{ maxWidth: 340, textAlign: "start" }}>
+            a Front-End & Full-Stack Engineer based in NYC 🗽
+          </H3>
+        </FlexColumn>
+      </FlexRow>
+    </FlexRow>
+  );
+};
